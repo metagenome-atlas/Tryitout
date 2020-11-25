943c9b9d414ff75fc8f461803b3639534f916938
kiesers <silas.kieser@gmail.com>
2020-11-25 12:10:18 +0100

    remove rqueired packages env instead of prodigal

--- setup.sh
+++ setup.sh
@@ -89,8 +89,8 @@ atlas run binning  $snakemake_args
 rm -r working_dir
 
 # remove environment so it will be installed again
-prodigal_env=$(grep prodigal  $db_dir/conda_envs/*.yaml | cut -f1 -d'.')
-rm -r $prodigal_env
+qc_env=$(grep pigz  $db_dir/conda_envs/*.yaml | cut -f1 -d'.')
+rm -r $qc_env
 
 conda deactivate
 unset CONDA_ALWAYS_YES
