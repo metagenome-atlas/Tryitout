09363329e9f3bdd2415ccb4abfc3efc667f5def4
silask <silas.kieser@gmail.com>
2020-11-27 14:07:06 +0100

    add script part2.sh

--- get_part2.sh
+++ get_part2.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
+#download and copy files to PArt2 folder
 git clone https://github.com/metagenome-atlas/Tutorial.git
 
-mv Tutorial Part2
+mkdir Part2
+mv Tutorial/R Tutorial/Python Tutorial/Example Part2
+rm -rf Tutorial
+
+# Remove output from Jupyter notebooks
+cd Part2
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace Python/Analyis_genome_abundances.ipynb
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace Python/Differential_abundance.ipynb
+jupyter nbconvert --ClearOutputPreprocessor.enabled=True --inplace Python/Draw_Tree.ipynb
