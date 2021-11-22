cf1df4daca4861d7210217b47d6ea483aedecd9a
silask <silas.kieser@gmail.com>
2021-11-22 14:23:16 +0100

    new setup

--- setup.sh
+++ setup.sh
@@ -1,17 +1,18 @@
 #!/usr/bin/env bash
-set -e
+set -eo pipefail
 
 ## install in docker with python 3
 # docker pull conda/miniconda3
 # docker run -i -t conda/miniconda3 /bin/bash
 
 # databases can be replaced with a shared location
-db_dir=/data/databases
+db_base_dir="."
+db_dir=$db_base_dir/databases
 
 this_folder=$(pwd)
 
 mkdir -p $db_dir
-cp human_genome.fasta $db_dir
+
 
 #set langauge locale
 #export LC_ALL=C.UTF-8
@@ -32,71 +33,58 @@ conda install -y mamba
 #update conda
 mamba update -n base -c defaults conda
 #conda init bash
-# install python3.6 in base environment
-mamba install python=3.6
-
-# install dependencies in env
-mamba env update -n base -f part2/jupyter_env.yaml
+# install python3.8 in base environment
+mamba install python=3.8
 
 
 #install nano if not available
 mamba install -y nano wget
-
-# install metagenome-atlas in its own environemnt
-mamba create -n adminenv metagenome-atlas
-
-source activate adminenv
-
-atlas --version
-
 nano --version
 
-# set up profile for each user
-mkdir -p /etc/xdg/snakemake #~/.config/snakemake/
-cp -r profile /etc/xdg/snakemake/Demo  #~/.config/snakemake/Demo
-
-#copy script
-#cp get_part2.sh ~
+# install metagenome-atlas in its own environemnt
+mamba create -n adminenv metagenome-atlas=2.8
+source activate adminenv
+atlas --version
 
 # go Home
 cd
 
 # get example data
-wget https://zenodo.org/record/3992790/files/test_reads.tar.gz
-tar -xzf test_reads.tar.gz
-
-rm -f test_reads.tar.gz # id this deleted by defautl?
+echo "Get example data"
+wget --quiet https://zenodo.org/record/3992790/files/test_reads.tar.gz
+tar -xzf test_reads.tar.gz --directory $db_base_dir
+rm -f test_reads.tar.gz
 
 echo "Test reads memory usage is"
-du -h -d1 test_reads
-
-mv test_reads $db_dir/..
-
-snakemake_args=" -w working_dir --profile Demo -p "
+du -h -d1 $db_base_dir/test_reads
 
 
-atlas init -w working_dir --db-dir $db_dir $db_dir/../test_reads
+echo "> contig_1\nAAAAAAAAAAATTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTTT" > $db_dir/human_genome.fasta
+
+
+## Test
+
+echo "Run atlas test"
+snakemake_args=" -w working_dir -j 2 --resources mem=5 java_mem=5 "
+
+
+atlas init -w working_dir --db-dir $db_dir $db_base_dir/test_reads
 
 cat working_dir/samples.tsv
 
 
-# run atlas this installs software via conda
+echo "run atlas this installs software via conda"
 atlas run assembly  $snakemake_args
 
 atlas run genecatalog  $snakemake_args
 
 atlas run binning  $snakemake_args
 
-#set +e # might trow an error in rule maxbin
-#atlas run binning  $snakemake_args
-#set -e
 
 # clean up so participants can restart from the beginning
 rm -r working_dir
 
-# remove environment so it will be installed again
-#qc_env=$(grep pigz  $db_dir/conda_envs/*.yaml | cut -f1 -d'.')
-#rm -r $qc_env
+
 
 conda deactivate
 unset CONDA_ALWAYS_YES
@@ -106,6 +94,3 @@ echo "Software memory usage is "
 du -h -d1 $db_dir
 
 
-# remove folder
-echo "remove $this_folder"
-rm -r $this_folder
